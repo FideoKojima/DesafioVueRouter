2a352de54d193a763b35fb5426cc77e8de4252ec
Luis Suarez <luisdcobain@gmail.com>
2024-11-11 17:09:55 -0300

    initialize Vue project with basic structure and configuration

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
+
+<template lang="">
+  <footer class="py-5 px-5">
+    <img src="/img/logo.png" class="mx-auto d-block" alt="suricata" />
+  </footer>
+</template>
